db8ec24fbb923b9d09253945554123a3ca2805e3
DuBento <duarte.bento2000@gmail.com>
2020-11-21 00:57:04 +0000

    cgi working, started planing

--- E3/web/testesSigma/app.cgi
+++ E3/web/testesSigma/app.cgi
@@ -1,26 +1,7 @@
 #!/usr/bin/python3
-import os
-# import syssys.path.insert(0, '/home/USERNAME/public_html/python/cgi-bin/venv/lib/python3.5'
-#                 '/site-packages')
+import sys
+sys.path.insert(0, '/afs/.ist.utl.pt/users/5/6/ist192456/.local/lib/python2.7/site-packages')
 from wsgiref.handlers import CGIHandler
 from app import app
 
-# class ProxyFix(object):
-#     def __init__(self, app):
-#         self.app = app
-    
-#     def __call__(self, environ, start_response):
-#         environ['SERVER_NAME'] = ""
-#         environ['SERVER_PORT'] = "80"
-#         environ['REQUEST_METHOD'] = "GET"
-#         environ['SCRIPT_NAME'] = ""
-#         environ['QUERY_STRING'] = ""
-#         environ['SERVER_PROTOCOL'] = "HTTP/1.1"
-#         return self.app(environ, start_response)
-
-# if __name__ == '__main__':
-#     app.wsgi_app = ProxyFix(app.wsgi_app)
-# CGIHandler().run(app)
-
-# app.wsgi_app = ProxyFix(app.wsgi_app)
 CGIHandler().run(app)
--- E3/web/testesSigma/app.py
+++ E3/web/testesSigma/app.py
@@ -1,5 +1,5 @@
-""" # !/usr/bin/python3 """
-from flask import Flask, request
+#!/usr/bin/python3
+from flask import Flask, request, url_for
 
 import psycopg2
 import psycopg2.extras
@@ -18,39 +18,127 @@ DB_CONNECTION_STRING = "host=%s dbname=%s user=%s password=%s" % (DB_HOST, DB_DA
 
 
 app = Flask(__name__)
+#app = Blueprint(request.base_url, __name__, url_prefix='/admin')
 
 @app.route('/index.html')
 @app.route('/')
 def home():
-    body = "<h1>Main Page</h1>\n"
-    body += "<p><a href='/instituicoes'>Instituicoes</p>\n"
-    body += "<p><a href='/medicos'>Medicos</p>\n"
-    body += "<p><a href='/prescricoes'>Prescricoes</p>\n"
-    body += "<p><a href='/analise'>Analise</p>\n"
+    body = "<h1>Pagina Inicial</h1>\n"
+    body += "<p><a href='%s'>Instituicoes</p>\n" %url_for('instituicao')
+    body += "<p><a href='%s'>Medicos</p>\n" %url_for('medico')
+    body += "<p><a href='%s'>Prescricoes</p>\n" %url_for('prescricao')
+    body += "<p><a href='%s'>Analise</p>\n" %url_for('analise')
     return composeHTML("Home Page", body)
 
 
-@app.route('/instituicoes/add')
-def instituicoes():
+@app.route('/instituicao')
+def instituicao():
+    body = "<h1>Intituicoes</h1>\n"
+    body += "<p><a href='%s'>Inserir</p>\n" %url_for('instituicaoAdd')
+    body += "<p><a href='%s'>Editar</p>\n" %url_for('instituicaoEdit')
+    body += "<p><a href='%s'>Remove</p>\n" %url_for('instituicaoRm')
+    return composeHTML("Inserir Instituicao", body)    
+
+@app.route('/instituicao/add')
+def instituicaoAdd():
     body = '<form action="update" method="post">\n'
     body += '\t<label for="fname">First name:</label><br>\n'
     body += '\t<input type="text" id="fname" name="fname" value="John"><br>\n'
     body += '\t<label for="lname">Last name:</label><br>\n'
     body += '\t<input type="text" id="lname" name="lname" value="Doe">\n'
     return composeHTML("add", body)
-    
+
+
+@app.route('/instituicao/edit')
+def instituicaoEdit():
+	return "Under Construction"
+
+
+@app.route('/instituicao/remove')
+def instituicaoRm():
+	body = '<form method="POST">\n'
+	body += '\t<label for="nome">Nome Instituicao:</label><br>\n'
+	body += '\t<input type="text" id="nome" name="nome"><br>\n'
+	body += '\t<input type="submit" value="Submit">\n'
+	body += '</form>\n'
+	return composeHTML("Remove", body)
+
 # /instituicoes/remove?nome="qq"&data=      {"nome": "qq"}
-@app.route('/instituicoes/remove', methods=['POST'])
-def remove_instituicoes():
-    if request.method == 'GET':
-        try:
-            print(request.form["nome"])
-            dbConn = psycopg2.connect(DB_CONNECTION_STRING)
-            cursor = dbConn.cursor(cursor_factory = psycopg2.extras.DictCursor)
-            query = "DELETE FROM instituicoes WHERE nome=(%s);"
-            cursor.execute(query, (request.args["nome"], ))
-        except Exception as e:
-            print(e)
+@app.route('/instituicao/remove', methods=['POST'])
+def instituicaoRmPOST():
+	if request.method == 'POST':
+		try:
+			print(request.form["nome"])
+			dbConn = psycopg2.connect(DB_CONNECTION_STRING)
+			cursor = dbConn.cursor(cursor_factory = psycopg2.extras.DictCursor)
+			query = "DELETE FROM instituicoes WHERE nome=(%s);"
+			cursor.execute(query, (request.args["nome"], ))
+			return cursor
+		except Exception as e:
+			print(e)
+	return "failsafe"
+	
+
+@app.route('/medico')
+def medico():
+    body = "<h1>Medicos</h1>\n"
+    body += "<p><a href='%s'>Inserir</p>\n" %url_for('medicoAdd')
+    body += "<p><a href='%s'>Editar</p>\n" %url_for('medicoEdit')
+    body += "<p><a href='%s'>Remove</p>\n" %url_for('medicoRm')
+    return composeHTML("Inserir Medico", body)
+
+@app.route('/medico/add')
+def medicoAdd():
+	return "Under Construction"
+
+@app.route('/medico/edit')
+def medicoEdit():
+	return "Under Construction"
+
+@app.route('/medico/remove')
+def medicoRm():
+	return "Under Construction"
+
+
+@app.route('/prescricao')
+def prescricao():
+    body = "<h1>Prescricoes</h1>\n"
+    body += "<p><a href='%s'>Inserir</p>\n" %url_for('prescricaoAdd')
+    body += "<p><a href='%s'>Editar</p>\n" %url_for('prescricaoEdit')
+    body += "<p><a href='%s'>Remove</p>\n" %url_for('prescricaoRm')
+    return composeHTML("Inserir Prescricao", body)
+
+@app.route('/prescricao/add')
+def prescricaoAdd():
+	return "Under Construction"
+
+@app.route('/prescricao/edit')
+def prescricaoEdit():
+	return "Under Construction"
+
+@app.route('/prescricao/remove')
+def prescricaoRm():
+	return "Under Construction"
+
+@app.route('/analise')
+def analise():
+    body = "<h1>Analises</h1>\n"
+    body += "<p><a href='%s'>Inserir</p>\n" %url_for('analiseAdd')
+    body += "<p><a href='%s'>Editar</p>\n" %url_for('analiseEdit')
+    body += "<p><a href='%s'>Remove</p>\n" %url_for('analiseRm')
+    return composeHTML("Inserir Analise", body)
+
+@app.route('/analise/add')
+def analiseAdd():
+	return "Under Construction"
+
+@app.route('/analise/edit')
+def analiseEdit():
+	return "Under Construction"
+
+@app.route('/analise/remove')
+def analiseRm():
+	return "Under Construction"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
