13e9685890e32dcbf503db3ab3762614d6647458
DuBento <duarte.bento2000@gmail.com>
2020-12-01 23:39:54 +0000

    RI-Analise

--- E4/RI.sql
+++ E4/RI.sql
@@ -15,7 +15,7 @@ begin
         ) AS A;
     
     if num_consultas >= 100 then
-        raise exception 'O medico % ja excedeu o limite de consultas para esta semana na instituicao %', new.num_cedula, new.nome_instituicao;
+        raise exception 'O medico % ja excedeu o limite de consultas para esta semana na instituicao %.', new.num_cedula, new.nome_instituicao;
     else
         return new;
     end if;
@@ -29,3 +29,60 @@ for each row execute procedure medico_100_consultas_proc();
 
 
 --RI-analise
+
+create or replace function especialidade_med_proc()
+returns trigger as
+$$
+begin
+
+    if new.num_cedula is not null AND 
+        new.num_doente is not null AND 
+        new._data is not null then
+
+        SELECT especialidade into new.especialidade
+        FROM medico
+        WHERE num_cedula = new.num_cedula;
+        raise notice 'Especialidade atualizada: %', new.especialidade;
+
+        return new;
+    end if;
+end
+$$language plpgsql;
+
+drop trigger especialidade_med on analise;
+create trigger especialidade_med before insert on analise
+for each row execute procedure especialidade_med_proc();
+
+
+
+
+create or replace function especialidade_med_proc()
+returns trigger as
+$$
+declare med_especial varchar;
+begin
+
+    if new.num_cedula is not null AND 
+        new.num_doente is not null AND 
+        new._data is not null then
+
+        SELECT especialidade into med_especial
+        FROM medico
+        WHERE num_cedula = new.num_cedula;
+
+        if new.especialidade != med_especial then
+            raise exception 'Especialidade do Medico (%) nao corresponde com a da analise (%).',
+                med_especial, new.especialidade;
+        else
+            return new;
+        end if;
+    else
+        return new;
+    end if;
+
+end
+$$language plpgsql;
+
+drop trigger especialidade_med on analise;
+create trigger especialidade_med before insert on analise
+for each row execute procedure especialidade_med_proc();
