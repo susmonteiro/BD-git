0d2038fd60c3762e4fdbf0977ebd873057848186
susmonteiro <s.moreno.monteiro@tecnico.ulisboa.pt>
2020-12-03 14:51:24 +0000

    chinopa v2

--- E4/star_schema.sql
+++ E4/star_schema.sql
@@ -29,42 +29,49 @@ create table d_instituicao(
 );
 
 create table f_presc_venda(
-    id_presc_venda serial not null,
+    id_presc_venda int not null,
     id_medico int not null,
     num_doente int not null,
-    id_data_registo serial not null,
-    id_inst serial not null,
+    id_data_registo int not null,
+    id_inst int not null,
     substancia varchar(50) not null,
     quant int not null,
-    foreign key(id_presc_venda) references prescricao(id_presc_venda), --??????
+    foreign key(id_presc_venda) references venda_farmacia(num_venda), --??????
     foreign key(id_medico) references medico(num_cedula),
     foreign key(id_data_registo) references d_tempo(id_tempo),
     foreign key(id_inst) references d_instituicao(id_inst),
-    primary key(id_presc_venda, id_medico, id_data_registo, id_inst)
+    primary key(id_presc_venda)
+    --primary key(id_presc_venda, id_medico, id_data_registo, id_inst)
 );
 
 create table f_analise(
-    id_analise serial not null,
-    id_medico serial not null,
+    id_analise int not null,
+    id_medico int not null,
     num_doente int not null,
     id_data_registo serial not null,
-    id_inst serial not null,
+    id_inst int not null,
     nome varchar(50) not null,
     quant int not null,
     foreign key(id_analise) references analise(num_analise),
     foreign key(id_medico) references medico(num_cedula),
     foreign key(id_data_registo) references d_tempo(id_tempo),
     foreign key(id_inst) references d_instituicao(id_inst),
-    primary key(id_analise, id_medico, id_data_registo, id_inst)
+    primary key(id_analise)
+    --primary key(id_analise, id_medico, id_data_registo, id_inst)
 );
 
 --Carregar dados nas tabelas
 insert into d_tempo(dia, dia_da_semana, semana, mes, trimestre, ano)
-    select distinct extract(DAY from _data) as day, extract(DOW from _data) das day_of_week,
-    extract(WEEK from _data) as week, extract(MONTH from _data) as month, extract(YEAR from _data) as year 
+    select distinct 
+    extract(DAY from _data) as dia, 
+    extract(DOW from _data) as dia_da_semana,
+    extract(WEEK from _data) as semana, 
+    extract(MONTH from _data) as mes, 
+    extract(QUARTER from _data) as trimestre,
+    extract(YEAR from _data) as ano 
     from consulta
     order by dia, mes, ano;
-    --como extrair o trimestre???
+    -- data da consulta ou ver data de analise e data de prescricao_venda???
 
 insert into d_instituicao(nome, tipo, num_regiao, num_concelho)
     select nome, tipo, num_regiao, num_concelho from instituicao;
@@ -74,4 +81,3 @@ insert into f_presc_venda(id_presc_venda, id_medico, num_doente, id_data_registo
 
 insert into f_analise(id_analise, id_medico, num_doente, id_data_registo, id_inst, nome, quant)
     --?????
-    
