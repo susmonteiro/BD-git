50ee6487dd19881f326fd44cf2543a14cc40a33c
susmonteiro <s.moreno.monteiro@tecnico.ulisboa.pt>
2020-12-04 01:13:25 +0000

    changes in constraints and fk

--- E4/star_schema.sql
+++ E4/star_schema.sql
@@ -4,11 +4,18 @@ drop table if exists d_instituicao cascade;
 drop table if exists f_presc_venda cascade;
 drop table if exists f_analise cascade;
 
+--Adicionar restricao
+ALTER TABLE prescricao_venda
+drop CONSTRAINT if exists Unique_num_venda;
+
+ALTER TABLE prescricao_venda
+ADD CONSTRAINT Unique_num_venda UNIQUE(num_venda);
+
 --Definir as tabelas
 create table d_tempo(
     id_tempo serial not null,
     dia int not null,
-    dia_da_semana int not null,
+    dia_da_semana int not null, 
     semana int not null,
     mes int not null,
     trimestre int not null,
@@ -18,13 +25,13 @@ create table d_tempo(
 
 create table d_instituicao(
     id_inst serial not null,
-    nome varchar(50) not null,
-    tipo varchar(25) not null,
+    nome varchar(128) not null,
+    tipo varchar(128) not null,
     num_regiao int not null,
     num_concelho int not null,
     foreign key(nome) references instituicao(nome),
-    foreign key(num_regiao) references regiao(num_concelho),
-    foreign key(num_concelho) references concelho(num_concelho),
+    foreign key(num_regiao) references regiao(num_regiao),
+    foreign key(num_regiao, num_concelho) references concelho(num_regiao, num_concelho),
     primary key(id_inst)
 );
 
@@ -34,7 +41,7 @@ create table f_presc_venda(
     num_doente int not null,
     id_data_registo int not null,
     id_inst int not null,
-    substancia varchar(50) not null,
+    substancia varchar(64) not null,
     quant int not null,
     foreign key(id_presc_venda) references prescricao_venda(num_venda), --??????
     foreign key(id_medico) references medico(num_cedula),
@@ -50,7 +57,7 @@ create table f_analise(
     num_doente int not null,
     id_data_registo int not null,
     id_inst int not null,
-    nome varchar(50) not null,
+    nome varchar(128) not null,
     quant int not null,
     foreign key(id_analise) references analise(num_analise),
     foreign key(id_medico) references medico(num_cedula),
@@ -59,26 +66,3 @@ create table f_analise(
     primary key(id_analise)
     --primary key(id_analise, id_medico, id_data_registo, id_inst)
 );
-
---Carregar dados nas tabelas
-insert into d_tempo(dia, dia_da_semana, semana, mes, trimestre, ano)
-    select distinct 
-    extract(DAY from _data) as dia, 
-    extract(DOW from _data) as dia_da_semana,
-    extract(WEEK from _data) as semana, 
-    extract(MONTH from _data) as mes, 
-    extract(QUARTER from _data) as trimestre,
-    extract(YEAR from _data) as ano 
-    from consulta
-    order by dia, mes, ano;
-    -- data da consulta ou ver data de analise e data de prescricao_venda???
-    -- escrever no relatorio: assumimos que id_data_registo refere-se a _data em ambas as tabelas
-
-insert into d_instituicao(nome, tipo, num_regiao, num_concelho)
-    select nome, tipo, num_regiao, num_concelho from instituicao;
-
-insert into f_presc_venda(id_presc_venda, id_medico, num_doente, id_data_registo, id_inst, substancia, quant)
-    --?????
-
-insert into f_analise(id_analise, id_medico, num_doente, id_data_registo, id_inst, nome, quant)
-    --?????
