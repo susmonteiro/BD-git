85428ca27296224839344a05741b8f087dd84918
DuBento <duarte.bento2000@gmail.com>
2020-12-08 20:22:19 +0000

    aditional to populate

---
+++ Discussao/addpopulate.sql
@@ -0,0 +1,37 @@
+
+
+insert into consulta values (8989, 901, '2020-01-9', 'testQ');
+insert into consulta values (8989, 902, '2020-01-9', 'testQ');
+insert into consulta values (8989, 903, '2020-01-9', 'testQ');
+insert into consulta values (8989, 904, '2020-01-9', 'testQ');
+insert into consulta values (8989, 905, '2020-01-9', 'testQ');
+insert into consulta values (8989, 906, '2020-01-9', 'testQ');
+insert into consulta values (8989, 907, '2020-01-9', 'testQ');
+
+
+
+insert into prescricao values (8989, 901, '2020-01-9', 'Alirocumabe', 9);
+insert into prescricao values (8989, 902, '2020-01-9', 'Alirocumabe', 9);
+insert into prescricao values (8989, 903, '2020-01-9', 'Alirocumabe', 9);
+insert into prescricao values (8989, 904, '2020-01-9', 'Alirocumabe', 9);
+insert into prescricao values (8989, 905, '2020-01-9', 'Alirocumabe', 9);
+insert into prescricao values (8989, 906, '2020-01-9', 'Alirocumabe', 9);
+insert into prescricao values (8989, 907, '2020-01-9', 'Alirocumabe', 9);
+
+
+insert into venda_farmacia values (801, '2020-01-9', 'Alirocumabe', 3, 23, 'testQ');
+insert into venda_farmacia values (802, '2020-01-9', 'Alirocumabe', 3, 23, 'testQ');
+insert into venda_farmacia values (803, '2020-01-9', 'Alirocumabe', 3, 23, 'testQ');
+insert into venda_farmacia values (804, '2020-01-9', 'Alirocumabe', 3, 23, 'testQ');
+insert into venda_farmacia values (805, '2020-01-9', 'Alirocumabe', 3, 23, 'testQ');
+insert into venda_farmacia values (806, '2020-01-9', 'Alirocumabe', 3, 23, 'testQ');
+insert into venda_farmacia values (807, '2020-01-9', 'Alirocumabe', 3, 23, 'testQ');
+
+
+insert into prescricao_venda values (8989, 901, '2020-01-9', 'Alirocumabe', 801);
+insert into prescricao_venda values (8989, 902, '2020-01-9', 'Alirocumabe', 802);
+insert into prescricao_venda values (8989, 903, '2020-01-9', 'Alirocumabe', 803);
+insert into prescricao_venda values (8989, 904, '2020-01-9', 'Alirocumabe', 804);
+insert into prescricao_venda values (8989, 905, '2020-01-9', 'Alirocumabe', 805);
+insert into prescricao_venda values (8989, 906, '2020-01-9', 'Alirocumabe', 806);
+insert into prescricao_venda values (8989, 907, '2020-01-9', 'Alirocumabe', 807);
