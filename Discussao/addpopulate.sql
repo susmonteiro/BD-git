963a08577b2b54ad9bf5d6bb94536f17625b91b1
susmonteiro <susmonteiro@PC-Su.localdomain>
2020-12-09 00:16:43 +0000

    new addpopulate

--- Discussao/addpopulate.sql
+++ Discussao/addpopulate.sql
@@ -1,4 +1,5 @@
-
+insert into instituicao values('testQ', 'farmacia', 3, 1106);
+insert into medico values (8989, 'nome', 'especialidade');
 
 insert into consulta values (8989, 901, '2020-01-9', 'testQ');
 insert into consulta values (8989, 902, '2020-01-9', 'testQ');
@@ -6,7 +7,9 @@ insert into consulta values (8989, 903, '2020-01-9', 'testQ');
 insert into consulta values (8989, 904, '2020-01-9', 'testQ');
 insert into consulta values (8989, 905, '2020-01-9', 'testQ');
 insert into consulta values (8989, 906, '2020-01-9', 'testQ');
-insert into consulta values (8989, 907, '2020-01-9', 'testQ');
+insert into consulta values (8989, 907, '2020-01-16', 'testQ');
+insert into consulta values (8989, 908, '2020-01-16', 'testQ');
+insert into consulta values (8989, 909, '2020-01-16', 'testQ');
 
 
 
@@ -16,7 +19,9 @@ insert into prescricao values (8989, 903, '2020-01-9', 'Alirocumabe', 9);
 insert into prescricao values (8989, 904, '2020-01-9', 'Alirocumabe', 9);
 insert into prescricao values (8989, 905, '2020-01-9', 'Alirocumabe', 9);
 insert into prescricao values (8989, 906, '2020-01-9', 'Alirocumabe', 9);
-insert into prescricao values (8989, 907, '2020-01-9', 'Alirocumabe', 9);
+insert into prescricao values (8989, 907, '2020-01-16', 'Alirocumabe', 9);
+insert into prescricao values (8989, 908, '2020-01-16', 'Alirocumabe', 9);
+insert into prescricao values (8989, 909, '2020-01-16', 'Alirocumabe', 9);
 
 
 insert into venda_farmacia values (801, '2020-01-9', 'Alirocumabe', 3, 23, 'testQ');
@@ -25,7 +30,9 @@ insert into venda_farmacia values (803, '2020-01-9', 'Alirocumabe', 3, 23, 'test
 insert into venda_farmacia values (804, '2020-01-9', 'Alirocumabe', 3, 23, 'testQ');
 insert into venda_farmacia values (805, '2020-01-9', 'Alirocumabe', 3, 23, 'testQ');
 insert into venda_farmacia values (806, '2020-01-9', 'Alirocumabe', 3, 23, 'testQ');
-insert into venda_farmacia values (807, '2020-01-9', 'Alirocumabe', 3, 23, 'testQ');
+insert into venda_farmacia values (807, '2020-01-16', 'Alirocumabe', 3, 23, 'testQ');
+insert into venda_farmacia values (808, '2020-01-16', 'Alirocumabe', 3, 23, 'testQ');
+insert into venda_farmacia values (809, '2020-01-16', 'Alirocumabe', 3, 23, 'testQ');
 
 
 insert into prescricao_venda values (8989, 901, '2020-01-9', 'Alirocumabe', 801);
@@ -34,4 +41,6 @@ insert into prescricao_venda values (8989, 903, '2020-01-9', 'Alirocumabe', 803)
 insert into prescricao_venda values (8989, 904, '2020-01-9', 'Alirocumabe', 804);
 insert into prescricao_venda values (8989, 905, '2020-01-9', 'Alirocumabe', 805);
 insert into prescricao_venda values (8989, 906, '2020-01-9', 'Alirocumabe', 806);
-insert into prescricao_venda values (8989, 907, '2020-01-9', 'Alirocumabe', 807);
+insert into prescricao_venda values (8989, 907, '2020-01-16', 'Alirocumabe', 807);
+insert into prescricao_venda values (8989, 908, '2020-01-16', 'Alirocumabe', 808);
+insert into prescricao_venda values (8989, 909, '2020-01-16', 'Alirocumabe', 809);
